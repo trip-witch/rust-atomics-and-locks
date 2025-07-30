419639fd86948e4fbb4a3afc3d5fd5c0ea314295
Esther Dow <esthera.bxtch@gmail.com>
2025-07-30 19:58:08 +0200

    add closure examples

--- chapters/basics-of-rust-concurrency/src/main.rs
+++ chapters/basics-of-rust-concurrency/src/main.rs
@@ -2,6 +2,8 @@ use std::thread;
 
 fn main() {
     first_example();
+    move_closure_example();
+    return_value_from_closure();
 }
 
 fn first_example() {
@@ -18,4 +20,29 @@ fn f() {
     println!("Hello from another thread!");
     let id = thread::current().id();
     println!("This is my thread id: {id:?}");
+}
+
+fn move_closure_example() {
+    let numbers = vec![1, 2, 3];
+    thread::spawn(move || {
+        for n in numbers {
+            println!("number {n}");
+        }
+    })
+    .join()
+    .unwrap();
+}
+
+fn return_value_from_closure() {
+    let numbers = Vec::from_iter(0..=1000);
+
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.into_iter().sum::<usize>();
+        sum / len
+    });
+
+    let average = t.join().unwrap();
+
+    println!("average: {average}");
 }
