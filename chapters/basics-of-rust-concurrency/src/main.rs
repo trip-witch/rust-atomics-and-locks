4e05bb0d2b2d8f0b2b3516dea55b2e950d7e3958
Esther Dow <esthera.bxtch@gmail.com>
2025-07-30 20:08:12 +0200

    Add scoped threads example

--- chapters/basics-of-rust-concurrency/src/main.rs
+++ chapters/basics-of-rust-concurrency/src/main.rs
@@ -4,9 +4,11 @@ fn main() {
     first_example();
     move_closure_example();
     return_value_from_closure();
+    scoped_threads_example();
 }
 
 fn first_example() {
+    println!("first example, basic threads executing function");
     let t1 = thread::spawn(f);
     let t2 = thread::spawn(f);
 
@@ -23,6 +25,7 @@ fn f() {
 }
 
 fn move_closure_example() {
+    println!("move closure example");
     let numbers = vec![1, 2, 3];
     thread::spawn(move || {
         for n in numbers {
@@ -34,6 +37,9 @@ fn move_closure_example() {
 }
 
 fn return_value_from_closure() {
+    println!(
+        "Return value from closure. Average is calculated in spawned thread and returned to main thread."
+    );
     let numbers = Vec::from_iter(0..=1000);
 
     let t = thread::spawn(move || {
@@ -45,4 +51,20 @@ fn return_value_from_closure() {
     let average = t.join().unwrap();
 
     println!("average: {average}");
+}
+
+fn scoped_threads_example() {
+    println!("Scoped threads example");
+    let numbers = vec![1, 2, 3];
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            println!("length: {}", numbers.len());
+        });
+        s.spawn(|| {
+            for n in &numbers {
+                println!("{n}");
+            }
+        });
+    });
 }
